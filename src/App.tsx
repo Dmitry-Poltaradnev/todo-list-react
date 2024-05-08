c2dbb7920b9ecb719044cd4a09c3af2faa74165f
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2024-05-08 15:03:58 +0400

    feat: new field

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,11 @@ function App() {
         {id:2, title:'SecondBook', isDone: false},
         {id:3, title:'ThirdBook', isDone: false}
     ]
+    const task4 = [
+        {id:1, title:'FirstBook', isDone: false},
+        {id:2, title:'SecondBook', isDone: false},
+        {id:3, title:'ThirdBook', isDone: false}
+    ]
 
 
     return (
