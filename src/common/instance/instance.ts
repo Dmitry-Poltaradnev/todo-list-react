c90637c6f62011e09ec74be6adfea2385242112e
User <poltaradnev@gmail.com>
2025-06-24 13:55:31 +0400

    Stop tracking .env and update .gitignore

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,14 @@
 import axios from "axios"
 
-const token = "48641b81-cc23-4c36-bc64-22b8910abddf"
+const baseURL = process.env.REACT_APP_BASE_URL
+const token = process.env.REACT_APP_AUTH_TOKEN
+const apiKey = process.env.REACT_APP_API_KEY
 
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1",
+  baseURL: baseURL,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${token}`,
-    "API-KEY": "0570d7e8-028b-4976-ac64-ded2181cd92b",
+    "API-KEY": apiKey,
   },
 })
